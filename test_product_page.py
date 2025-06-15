68cac19d846a268e84a3359c292f38f3064f16a4
Виктория <brizyriot@yandex.ru>
2025-06-15 14:39:24 +0300

    Update

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,8 @@
 import pytest
 import time
-from pages.product_page import ProductPage
-from pages.login_page import LoginPage
-from pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 product_any_link  = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
